281eaea647ab2d0b1a9f921099632ddcb44c8396
Alessandra-Casu <alessandracasu919@gmail.com>
2023-05-22 17:24:30 +0200

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <script>
-export default {};
+import CharacterCard from "./components/CharacterCard.vue";
+import CharacterList from "./components/CharacterList.vue";
+import ResultsMessage from "./components/ResultsMessage.vue";
+import axios from "axios";
+import { store } from "./store";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+  components: {
+    CharacterList,
+    ResultsMessage,
+  },
+  created() {
+    //qui fare la richiesta al'api
+    axios
+      .get("https://db.ygoprodeck.com/api/v7/cardinfo.php")
+      .then((ajax) => (this.store.characterList = ajax.data.results));
+  },
+};
 </script>
 
 <template>
-  <AppHeader></AppHeader>
+  <AppHeader>
+    <h1>Yu-Gi-Oh Api</h1>
+  </AppHeader>
   <main>
+    <ResultsMessage></ResultsMessage>
     <CharacterCard></CharacterCard>
     <CharacterList></CharacterList>
-    <ResultsMessage></ResultsMessage>
   </main>
 </template>
 
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: {
+    characterData: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <img :src="characterData.image" :alt="characterData.name" />
+    <h2>{{ characterData.name }}</h2>
+    <div class="type">{{ character.type }}</div>
+  </div>
+</template>
+
+<style lang="scss">
+.card {
+  padding: 2rem 1rem;
+  border: 2px solid black;
+}
+img {
+  max-width: 100%;
+}
+</style>
