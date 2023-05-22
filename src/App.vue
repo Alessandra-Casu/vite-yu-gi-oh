1f8f44001d3b633bec8c7ddfbb5ec4762d2b1769
Alessandra-Casu <alessandracasu919@gmail.com>
2023-05-22 16:51:29 +0200

    add struct

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <AppHeader></AppHeader>
+  <main>
+    <CharacterCard></CharacterCard>
+    <CharacterList></CharacterList>
+    <ResultsMessage></ResultsMessage>
+  </main>
+</template>
+
+<style lang="scss"></style>
